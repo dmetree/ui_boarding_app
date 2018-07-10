c86f1a4d2584636f5d85d313c1c05f3cf5848265
BondarenkoDmitry <demianbondarenko@gmail.com>
2018-07-10 12:31:06 +0300

    Adding fake data

--- app/src/main/java/com/dvb/ui_boarding_app/BoardingPassInfo.java
+++ app/src/main/java/com/dvb/ui_boarding_app/BoardingPassInfo.java
@@ -1,6 +1,27 @@
 package com.dvb.ui_boarding_app;
 
+import java.util.concurrent.TimeUnit;
+import java.sql.Timestamp;
+
 public class BoardingPassInfo {
 
+    public String passengerName;
+    public String flightCode;
+    public String originCode;
+    public String destCode;
 
+    public Timestamp boardingTime;
+    public Timestamp departureTime;
+    public Timestamp arrivalTime;
+
+    public String departureTerminal;
+    public String departureGate;
+    public String seatNumber;
+
+    public int barCodeImageResource;
+
+    public long getMinutesUntilBoarding() {
+        long millisUntilBoarding = boardingTime.getTime() - System.currentTimeMillis();
+        return TimeUnit.MILLISECONDS.toMinutes(millisUntilBoarding);
+    }
 }
--- app/src/main/java/com/dvb/ui_boarding_app/MainActivity.java
+++ app/src/main/java/com/dvb/ui_boarding_app/MainActivity.java
@@ -1,13 +1,28 @@
 package com.dvb.ui_boarding_app;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dvb.ui_boarding_app.databinding.ActivityMainBinding;
+import com.dvb.ui_boarding_app.utilities.FakeDataUtils;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        BoardingPassInfo fakeBoardingInfo = FakeDataUtils.generateFakeBoardingPassInfo();
     }
+
+
+    private void displayBoardingPassInfo(BoardingPassInfo info){
+
+    }
+
 }
